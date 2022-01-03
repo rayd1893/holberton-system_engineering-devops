cdc2b7c9d76efb306b57aac76d355961853173fb
rayd1893 <rayd1893@gmail.com>
2022-01-03 01:03:27 -0500

    Change owner and group

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,7 +3,7 @@
 file {'Create a file':
     path    => '/tmp/school',
     mode    =>  '0744',
-    owner   =>  'www-data',
-    group   =>  'www-data',
+    owner   =>  www-data,
+    group   =>  www-data,
     content =>  'I love Puppet',
 }
