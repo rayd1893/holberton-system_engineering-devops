f44e3c91cc431f67596d62f7844b98ad54d5658d
rayd1893 <rayd1893@gmail.com>
2022-01-03 18:00:53 -0500

    Install puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 # Install puppet-lint
 
 package { 'puppet-lint':
-  ensure  =>  '2.5',
+  ensure  =>  '2.5.0',
   provider  =>  gem,
+  source  =>  'http://rubygems.org',
+  name     => 'puppet-lint',
 }
